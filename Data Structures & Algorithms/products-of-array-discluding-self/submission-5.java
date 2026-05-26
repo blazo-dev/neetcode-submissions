d7078f297dfbc749d1f561bce6e553f606d641cc
Bryan Lazo <132369592+blazo-dev@users.noreply.github.com>
2026-05-26 13:44:16 -0500

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int prefix = 1;
+        int posfix = 1;
+        int[] output = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = prefix;
+            prefix *= nums[i];
+        }
+
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= posfix;
+            posfix *= nums[i];
+        }
+
+        return output;
+    }
+}
