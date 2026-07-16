91eae0ca8f6eae6ccb04a66481420ebe922d177b
Bryan Lazo <132369592+blazo-dev@users.noreply.github.com>
2026-07-16 10:09:16 -0500

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # Need to track indexes for each num
+        idxTrack = {}
+
+        # Then loop over the numbers 
+        for idx in range(0, len(nums)):
+            # Get complement
+            comp = target - nums[idx]
+
+            # Check for complement in track, and if comIdx return 
+            compIdx = idxTrack.get(comp, -1)
+
+            if compIdx >= 0:
+                return [compIdx, idx]
+            
+            idxTrack.setdefault(nums[idx], idx)
+
+        return []
