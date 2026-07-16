564af7b78bd4af0c7a82e954cd5b0e549dfd2cf1
Bryan Lazo <132369592+blazo-dev@users.noreply.github.com>
2026-07-16 07:35:39 -0500

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # Track seen elements
+        seen = {}
+
+        # Go over all elements
+        for num in nums:
+
+            # If seen return true
+            if(seen.get(num)):
+                return True
+
+            # If not seen element add to dic
+            seen.setdefault(num, 1)
+
+        return False
