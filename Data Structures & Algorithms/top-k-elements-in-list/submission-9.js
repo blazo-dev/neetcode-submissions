f93335c4ad14fb2fe4700494e7f234ebb2ab0756
Bryan Lazo <132369592+blazo-dev@users.noreply.github.com>
2026-07-22 07:38:40 -0500

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const counter = {}
+        const freq = Array.from(Array(nums.length + 1), _ => [])
+
+        for(let n of nums) {
+            counter[n] = (counter[n] || 0) + 1;
+        }
+
+        for (const [num, count] of Object.entries(counter)) {
+            freq[count].push(parseInt(num))
+        }
+
+        const res = []
+        for(let i = freq.length - 1; i >= 0; i--) {
+            for(let j  = 0; j < freq[i].length; j++) {
+                res.push(freq[i][j])
+                if(res.length === k) return res;
+            }
+        }
+
+        return res;
+    }
+}
