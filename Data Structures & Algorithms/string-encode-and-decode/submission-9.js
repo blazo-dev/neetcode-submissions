5dbc5c2720b3a2fad2972bb3fd66be4477641dd2
Bryan Lazo <132369592+blazo-dev@users.noreply.github.com>
2026-07-22 20:21:12 -0500

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encoded = ""
+
+        for(const str of strs) {
+            encoded += `${str.length}#${str}` 
+        }
+        
+        console.log({ encoded })
+
+        return encoded
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let long = "";
+        const res = []
+
+        for(let i = 0; i < str.length; i++) {
+            if(str[i] === "#"){
+                let numericLong = Number(long);
+
+                res.push(str.substring(i+1, i + numericLong + 1))
+                long = ""
+                i += numericLong;
+                continue;
+            }
+
+            long += str[i];
+        }
+
+        return res;
+    }
+}
