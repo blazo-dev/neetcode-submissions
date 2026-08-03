d1f5366846cbab1bb6c3b12de70662419c274c84
Bryan Lazo <132369592+blazo-dev@users.noreply.github.com>
2026-08-03 07:19:24 -0500

    Add: duplicate-integer - submission-17

---
+++ Data Structures & Algorithms/duplicate-integer/submission-17.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const seen = {};
+
+        for(const num of nums) {
+
+            seen[num] = seen[num] ? seen[num] + 1 : 1;
+
+            if(seen[num] > 1) return true;
+
+        }
+
+        return false;
+    }
+}
