0e76b0338e3f3cb63e02745ee13ae90560a27d0b
Bryan Lazo <132369592+blazo-dev@users.noreply.github.com>
2026-08-04 07:20:32 -0500

    Add: is-anagram - submission-33

---
+++ Data Structures & Algorithms/is-anagram/submission-33.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) return false;
+
+        const sFreq = {};
+        const tFreq = {};
+
+        for (const ch of s) sFreq[ch] = sFreq[ch] + 1 || 1;
+        for (const ch of t) tFreq[ch] = tFreq[ch] + 1 || 1;
+        
+        for (const ch of s) if (sFreq[ch] != tFreq[ch]) return false;
+
+        return true;
+    }
+}
