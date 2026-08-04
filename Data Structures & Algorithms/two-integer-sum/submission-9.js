4b16915856d25be09380faf5eba020b58a149986
Bryan Lazo <132369592+blazo-dev@users.noreply.github.com>
2026-08-04 07:48:24 -0500

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const seen = {};
+
+        for (let i = 0; i < nums.length; i++) {
+            const comp = target - nums[i];
+
+            if (seen[comp] >= 0) return [seen[comp], i];
+
+            seen[nums[i]] = i;
+        }
+
+        return [];
+    }
+}
